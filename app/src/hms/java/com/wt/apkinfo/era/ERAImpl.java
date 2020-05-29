10e2d9e6474b39f44050d8471842a1a9e70b4115
kenumir <kenumir@gmail.com>
2020-05-29 14:18:05 +0200

    HMS version include some GMS libraries working on huawei devices (no GMS)

--- app/src/hms/java/com/wt/apkinfo/era/ERAImpl.java
+++ app/src/hms/java/com/wt/apkinfo/era/ERAImpl.java
@@ -2,24 +2,37 @@ package com.wt.apkinfo.era;
 
 import android.content.Context;
 
-import com.huawei.agconnect.crash.AGConnectCrash;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
 
 public class ERAImpl {
 
 	public static void setString(String key, String value) {
-		//Crashlytics.setString(key, value);
+		try {
+			FirebaseCrashlytics.getInstance().setCustomKey(key, value);
+		} catch (Exception e) {
+			// ignore
+		}
 	}
 
 	public static void testError(Context ctx) {
-		AGConnectCrash.getInstance().testIt(ctx);
+		//AGConnectCrash.getInstance().testIt(ctx);
 	}
 
 	public static void logException(Throwable e) {
-		//AGConnectCrash.getInstance().
+		try {
+			FirebaseCrashlytics.getInstance().recordException(e);
+		} catch (Exception ee) {
+			// ignore
+		}
 	}
 
 	public static void log(String s) {
-
+		try {
+			FirebaseCrashlytics.getInstance().log(s);
+		} catch (Exception ee) {
+			// ignore
+		}
 	}
 
 }
