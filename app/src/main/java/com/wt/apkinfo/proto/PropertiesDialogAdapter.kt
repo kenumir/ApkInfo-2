25f4bf81c87fbae7dfaebe867438ad1d099cb466
kenumir <kenumir@gmail.com>
2020-06-02 12:00:51 +0200

    Copy to clipboard selected item data. PL translation.

--- app/src/main/java/com/wt/apkinfo/proto/PropertiesDialogAdapter.kt
+++ app/src/main/java/com/wt/apkinfo/proto/PropertiesDialogAdapter.kt
@@ -1,15 +1,19 @@
 package com.wt.apkinfo.proto
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.wt.apkinfo.BuildConfig
 import com.wt.apkinfo.R
 
-class PropertiesDialogAdapter(list: ArrayList<String>) : RecyclerView.Adapter<PropertiesDialogAdapter.Holder>() {
-
-    private val items = list
+class PropertiesDialogAdapter(private val items: ArrayList<String>) : RecyclerView.Adapter<PropertiesDialogAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         return Holder(LayoutInflater.from(parent.context).inflate(R.layout.item_property, parent, false))
@@ -27,6 +31,29 @@ class PropertiesDialogAdapter(list: ArrayList<String>) : RecyclerView.Adapter<Pr
         private val text1 = itemView.findViewById<TextView>(R.id.text1)
         private val text2 = itemView.findViewById<TextView>(R.id.text2)
         private val text3 = itemView.findViewById<TextView>(R.id.text3)
+        init {
+            itemView.setOnClickListener{
+                var text = ""
+                if (text1.visibility == View.VISIBLE) {
+                    text = text1.text.toString()
+                }
+                if (text2.visibility == View.VISIBLE) {
+                    text = text + (if (text.isNotEmpty()) " - " else "") +  text2.text.toString()
+                }
+                if (text3.visibility == View.VISIBLE) {
+                    text = text + (if (text.isNotEmpty()) " - " else "") +  text3.text.toString()
+                }
+
+                val clipboard: ClipboardManager = it.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("ApkInfo", text)
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(it.context, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show()
+
+                if (BuildConfig.DEBUG) {
+                    Log.i("ApkInfo", "Copy: $text")
+                }
+            }
+        }
         fun update(text: String) {
             val lines = text.split("\n")
             when(lines.size) {
