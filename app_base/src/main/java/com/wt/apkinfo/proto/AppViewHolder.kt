e8c8069fc069c176cdd6984bdce8aee64501c447
kenumir <kenumir@gmail.com>
2024-09-04 23:28:35 +0200

    Add update date time

--- app_base/src/main/java/com/wt/apkinfo/proto/AppViewHolder.kt
+++ app_base/src/main/java/com/wt/apkinfo/proto/AppViewHolder.kt
@@ -7,13 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.wt.apkinfo.base.R
 import com.wt.apkinfo.data.ApplicationEntryInfo
 import com.wt.apkinfo.data.images.ImageLoader
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AppViewHolder(itemView: View, mOnAppListItemClick: OnAppListItemClick?) : RecyclerView.ViewHolder(itemView) {
 
     private val icon: ImageView = itemView.findViewById(R.id.icon)
     private val text1: TextView = itemView.findViewById(R.id.text1)
     private val text2: TextView = itemView.findViewById(R.id.text2)
+    private val text3: TextView = itemView.findViewById(R.id.text3)
     private var mApplicationEntryInfo: ApplicationEntryInfo? = null
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
 
     init {
         itemView.setOnClickListener{
@@ -27,6 +31,7 @@ class AppViewHolder(itemView: View, mOnAppListItemClick: OnAppListItemClick?) : 
         data?.let {
             text1.text = it.name
             text2.text = it.pkg
+            text3.text = if (it.date == 0L) "-" else dateFormat.format(it.date)
             ImageLoader.get(icon.context).load(it.icon, icon)
         }
     }
