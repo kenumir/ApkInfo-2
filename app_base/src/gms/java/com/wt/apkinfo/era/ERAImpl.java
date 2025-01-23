b81fdbdbfd6f65b39fe75cd88e39080951b9feda
kenumir <kenumir@gmail.com>
2025-01-23 21:43:03 +0100

    Remove CR

--- app_base/src/gms/java/com/wt/apkinfo/era/ERAImpl.java
+++ app_base/src/gms/java/com/wt/apkinfo/era/ERAImpl.java
@@ -3,17 +3,12 @@ package com.wt.apkinfo.era;
 import android.content.Context;
 
 import com.bugsnag.android.Bugsnag;
-import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 
 public class ERAImpl {
 
 	public static void setString(String key, String value) {
-		try {
-			FirebaseCrashlytics.getInstance().setCustomKey(key, value);
-		} catch (Exception e) {
-			// ignore
-		}
+
 	}
 
 	public static void testError(Context ctx) {
@@ -22,20 +17,10 @@ public class ERAImpl {
 	}
 
 	public static void logException(Throwable e) {
-		try {
-			FirebaseCrashlytics.getInstance().recordException(e);
-		} catch (Exception ee) {
-			// ignore
-		}
 		Bugsnag.notify(e);
 	}
 
 	public static void log(String s) {
-		try {
-			FirebaseCrashlytics.getInstance().log(s);
-		} catch (Exception ee) {
-			// ignore
-		}
 		Bugsnag.leaveBreadcrumb(s);
 	}
 
